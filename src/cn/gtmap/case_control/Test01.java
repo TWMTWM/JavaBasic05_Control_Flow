c44da30e3623c63f100c52a16404cbb949922db8
TWM <15152806231@163.com>
2020-01-07 09:04:52 +0800

    switch

---
+++ src/cn/gtmap/case_control/Test01.java
@@ -0,0 +1,54 @@
+package cn.gtmap.case_control;
+
+public class Test01 {
+	/**
+	 * 1、switch case
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//如果使用if else
+		int day = 5;
+		if(day==1)
+			System.out.println("星期一");
+		else if(day==2)
+			System.out.println("星期二");
+		else if(day==3)
+			System.out.println("星期三");
+		else if(day==4)
+			System.out.println("星期四");
+		else if(day==5)
+			System.out.println("星期五");
+		else if(day==6)
+			System.out.println("星期六");
+		else if(day==7)
+			System.out.println("星期天");
+		else 
+			System.out.println("输的是啥JB玩意");
+		//如果使用switch case
+		switch(day){
+			case 1:
+				System.out.println("星期一");
+				break;
+			case 2:
+				System.out.println("星期二");
+				break;
+			case 3:
+				System.out.println("星期三");
+				break;
+			case 4:
+				System.out.println("星期四");
+				break;
+			case 5:
+				System.out.println("星期五");
+				break;
+			case 6:
+				System.out.println("星期六");
+				break;
+			case 7:
+				System.out.println("星期日");
+				break;
+			default:
+				System.out.println("输的是啥JB玩意");	
+		}
+	}
+}
