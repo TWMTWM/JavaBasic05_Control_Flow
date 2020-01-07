326f16b60e2b30c542168fab5765d72aac980932
TWM <15152806231@163.com>
2020-01-07 09:27:21 +0800

    while:条件为true时 重复执行

---
+++ src/cn/gtmap/while_control/Test01.java
@@ -0,0 +1,19 @@
+package cn.gtmap.while_control;
+
+public class Test01 {
+	/**
+	 * 1、while 条件为true 时重复执行
+	 */
+	public static void main(String[] args) {
+		//只要while中的表达式成立，就会不断地循环执行
+		//打印0到4
+		int i = 0;
+		while(i<5){
+			System.out.println(i);
+			i++;
+		}
+		
+		
+	}
+
+}
