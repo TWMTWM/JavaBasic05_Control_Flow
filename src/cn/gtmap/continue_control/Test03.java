23032d24b8b871884c83a0df16045b86519ae735
TWM <15152806231@163.com>
2020-01-07 10:31:04 +0800

    break:直接结束当前for循环

---
+++ src/cn/gtmap/continue_control/Test03.java
@@ -0,0 +1,14 @@
+package cn.gtmap.continue_control;
+
+public class Test03 {
+	/**
+	 * break:结束循环
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			if(0==i%2)
+				break;//如果是双数，直接结束循环
+			System.out.println(i);
+		}
+	}
+}
