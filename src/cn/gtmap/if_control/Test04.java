73bd99c1e94b2721325743ccec2dfd8a43548e25
TWM <15152806231@163.com>
2020-01-07 08:42:42 +0800

    if else

---
+++ src/cn/gtmap/if_control/Test04.java
@@ -0,0 +1,15 @@
+package cn.gtmap.if_control;
+
+public class Test04 {
+	/**
+	 * 4、if else
+	 * 	else 代表不成立的情况
+	 */
+	public static void main(String[] args) {
+		boolean b = false;
+		if(b)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}
